2a664b4904ac407630bd5fc1a94f84ec20288424
Daniel Rabago Prades <danielrabagoprades@gmail.com>
2024-01-29 21:40:24 +0100

    Changed the vhost name

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const vhost = require("vhost");
 
 let app = express();
 
-app.use(vhost("libros.ovh",
+app.use(vhost("vps-4c070e59.vps.ovh.net",
     require("./../vps-test/index").app));
 
 app.listen(3000);
